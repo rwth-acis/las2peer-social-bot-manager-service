1e9fb1c78e6af55d65bb2d9b2dccee6d6917c4c9
Alexander Neumann <neumann@dbis.rwth-aachen.de>
2020-04-20 09:52:32 +0200

    utf-8

--- src/main/i5/las2peer/services/socialBotManagerService/chat/StaticResponses.java
+++ src/main/i5/las2peer/services/socialBotManagerService/chat/StaticResponses.java
@@ -1,8 +1,8 @@
 package i5.las2peer.services.socialBotManagerService.chat;
 
 public class StaticResponses {
-	public static String dataAskingQuestion = "Könnte ich dich nach ein paar personenbezogene Daten fragen?";
-	public static String dataAskingAccepptedResponse = "Danke, könntest du mir dann bitte kommagetrennt dein Alter, Geschlecht und dein Hochschulsemester nennen?";
+	public static String dataAskingQuestion = "KÃ¶nnte ich dich nach ein paar personenbezogene Daten fragen?";
+	public static String dataAskingAccepptedResponse = "Danke, kÃ¶nntest du mir dann bitte kommagetrennt dein Alter, Geschlecht und dein Hochschulsemester nennen?";
 	public static String dataAskingPositiveFinish = "Danke!";
 	public static String dataAskingRejectedResponse = "Okay";
 }
