dea450d3db9e57b994e52dbb21eba3fd57ea2b31
Aran30 <aaron30@live.be>
2021-04-01 12:05:24 +0200

    Hotfix leadsTo 2 correct

--- src/main/i5/las2peer/services/socialBotManagerService/model/Messenger.java
+++ src/main/i5/las2peer/services/socialBotManagerService/model/Messenger.java
@@ -269,9 +269,23 @@ public class Messenger {
 							}
 						}
 					} else {
-						System.out.println(
-								intent.getKeyword() + " not detected with " + intent.getConfidence() + " confidence.");
-						state = this.knownIntents.get("default");
+						if (state.getFollowingMessages().get("") != null) {
+							System.out.println("Empty leadsTo");
+							if (message.getFileBody() != null) {
+								if (state.getFollowingMessages().get("").expectsFile()) {
+									state = state.getFollowingMessages().get("");
+								} else {
+									state = this.knownIntents.get("default");
+								}
+							} else {
+								state = state.getFollowingMessages().get("");
+								stateMap.put(message.getChannel(), state);
+							}
+						} else {
+							System.out.println(intent.getKeyword() + " not detected with " + intent.getConfidence()
+									+ " confidence.");
+							state = this.knownIntents.get("default");
+						}
 						// System.out.println(state.getIntentKeyword() + " set");
 					}
 					// If a user sends a file, without wanting to use intent extraction on the name, then intent
