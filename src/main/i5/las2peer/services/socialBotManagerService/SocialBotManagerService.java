4cd900a897864e750aa749ffe41e6720d0629c05
Aran30 <aaron30@live.be>
2021-05-07 00:06:12 +0200

    forget to push method ups

--- src/main/i5/las2peer/services/socialBotManagerService/SocialBotManagerService.java
+++ src/main/i5/las2peer/services/socialBotManagerService/SocialBotManagerService.java
@@ -1729,4 +1729,35 @@ public class SocialBotManagerService extends RESTService {
 			return resp;
 		}
 	}
+
+	public static String encryptThisString(String input) {
+		try {
+			// getInstance() method is called with algorithm SHA-384
+			MessageDigest md = MessageDigest.getInstance("SHA-384");
+
+			// digest() method is called
+			// to calculate message digest of the input string
+			// returned as array of byte
+			byte[] messageDigest = md.digest(input.getBytes());
+
+			// Convert byte array into signum representation
+			BigInteger no = new BigInteger(1, messageDigest);
+
+			// Convert message digest into hex value
+			String hashtext = no.toString(16);
+
+			// Add preceding 0s to make it 32 bit
+			while (hashtext.length() < 32) {
+				hashtext = "0" + hashtext;
+			}
+
+			// return the HashText
+			return hashtext;
+		}
+
+		// For specifying wrong message digest algorithms
+		catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
