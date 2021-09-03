f86ca4ff60321d96f667583808887737ba1e916a
Aran30 <aaron30@live.be>
2021-09-03 10:47:40 +0200

    added changes to make compatible with java 14

--- src/main/i5/las2peer/services/socialBotManagerService/parser/creation/CreatorBot.java
+++ src/main/i5/las2peer/services/socialBotManagerService/parser/creation/CreatorBot.java
@@ -3,9 +3,9 @@ package i5.las2peer.services.socialBotManagerService.parser.creation;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
-import javax.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlElementWrapper;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
 import i5.las2peer.services.socialBotManagerService.parser.creation.function.CreatorFunction;
 import i5.las2peer.services.socialBotManagerService.parser.creation.messenger.Messenger;
@@ -80,9 +80,9 @@ public class CreatorBot {
 	}
 
 	public void addFunction(CreatorFunction function) {
-		if(this.function == null)
+		if (this.function == null)
 			this.function = new ArrayList<>();
 		this.function.add(function);
 	}
-	
+
 }
