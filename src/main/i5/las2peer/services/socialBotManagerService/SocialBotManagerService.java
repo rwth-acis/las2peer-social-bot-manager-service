edf19195fca24208f6c82b9fa5fbcb7e07ecc713
Theresa Täuber <theresa.taeuber@rwth-aachen.de>
2021-10-20 13:46:28 +0200

    pipeline fix

--- src/main/i5/las2peer/services/socialBotManagerService/SocialBotManagerService.java
+++ src/main/i5/las2peer/services/socialBotManagerService/SocialBotManagerService.java
@@ -51,7 +51,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-import import com.github.seratch.jslack.Slack;
+import com.github.seratch.jslack.Slack;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.gson.Gson;
