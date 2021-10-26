a76d49a65e9fc45e6348ec021a1629c7ea4d7251
Aran30 <aaron30@live.be>
2021-10-26 11:25:56 +0200

    fixed docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -84,7 +84,7 @@ fi
 
 # prevent glob expansion in lib/*
 set -f
-LAUNCH_COMMAND='java -cp lib/* i5.las2peer.tools.L2pNodeLauncher -s service -p '"${LAS2PEER_PORT} ${SERVICE_EXTRA_ARGS}"
+LAUNCH_COMMAND='java -cp lib/* -jar lib/las2peer-bundle-'"${CORE_VERSION}"'.jar -s service -p '"${LAS2PEER_PORT} ${SERVICE_EXTRA_ARGS}"
 if [[ ! -z "${BOOTSTRAP}" ]]; then
     LAUNCH_COMMAND="${LAUNCH_COMMAND} -b ${BOOTSTRAP}"
 fi
