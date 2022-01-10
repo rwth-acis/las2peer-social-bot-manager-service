d419b92589bd571a0a56b64410356c8ad32475f0
Aran30 <aaron30@live.be>
2022-01-10 16:18:50 +0100

    deleted thrash

--- social-bot-manager/src/main/java/i5/las2peer/services/socialBotManagerService/chat/SlackChatMediator.java
+++ social-bot-manager/src/main/java/i5/las2peer/services/socialBotManagerService/chat/SlackChatMediator.java
@@ -376,7 +376,7 @@ public class SlackChatMediator extends EventChatMediator {
 		try {
 			response2 = slack.methods(authToken).filesUpload(req -> req.channels(channels).file(f)
 					.content("Pretty stuff").filename(f.getName()).title(f.getName()).initialComment(text));
-			System.out.println("File sendt: " + response2.isOk()  + text);
+			System.out.println("File sent: " + response2.isOk()  + text);
 		} catch (IOException | SlackApiException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
