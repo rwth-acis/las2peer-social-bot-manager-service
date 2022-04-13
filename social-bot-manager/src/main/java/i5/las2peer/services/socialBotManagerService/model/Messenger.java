a5b9904f690be47bc1caca05d9b462d5258ea871
Aran30 <aaron30@live.be>
2022-04-13 15:55:24 +0200

    further logs

--- social-bot-manager/src/main/java/i5/las2peer/services/socialBotManagerService/model/Messenger.java
+++ social-bot-manager/src/main/java/i5/las2peer/services/socialBotManagerService/model/Messenger.java
@@ -211,6 +211,7 @@ public class Messenger {
 	public void handleMessages(ArrayList<MessageInfo> messageInfos, Bot bot) {
 		Vector<ChatMessage> newMessages = this.chatMediator.getMessages();
 		for (ChatMessage message : newMessages) {
+			System.out.println("Start of handling message in messenger class");
 			try {
 				// // If a channel/user pair still isn't assigned to a state, assign it to null
 				// if (this.stateMap.get(message.getChannel()) == null) {
