ee56659e0b5870bb8808e927a745ea87190ccd5c
Aran30 <50965489+Aran30@users.noreply.github.com>
2023-05-24 20:44:04 +0200

    furhter debug logs cause i really dont get it

--- social-bot-manager/src/main/java/i5/las2peer/services/socialBotManagerService/SocialBotManagerService.java
+++ social-bot-manager/src/main/java/i5/las2peer/services/socialBotManagerService/SocialBotManagerService.java
@@ -3192,7 +3192,7 @@ public class SocialBotManagerService extends RESTService {
 												body.put(key, messageInfo.getMessage().getChannel());
 											}
 											if(body.get(key) != null && body.get(key).toString().contains("[")&& body.get(key).toString().contains("]")){
-												System.out.println("replacing " + key);
+												System.out.println("replacing " + key +" "+body.get(key).toString() + " "+ m.replaceVariables(channel,body.get(key).toString()));
 												body.put(key, m.replaceVariables(channel,body.get(key).toString()));
 											}
 										}
