8764940784e8d7e3f172d11d24d8635ccc864232
lakhoune <a.b.lakhoune@gmail.com>
2023-08-15 18:58:46 +0200

    remove weird test classes

--- social-bot-manager/src/test/java/java/i5/las2peer/services/socialBotManagerService/chat/StateContainer.java
+++
@@ -1,9 +0,0 @@
-package i5.las2peer.services.socialBotManagerService.chat;
-
-import i5.las2peer.services.socialBotManagerService.model.IncomingMessage;
-
-public abstract class StateContainer {
-
-	public abstract IncomingMessage getIntentState(String channel, String user);
-	
-}
