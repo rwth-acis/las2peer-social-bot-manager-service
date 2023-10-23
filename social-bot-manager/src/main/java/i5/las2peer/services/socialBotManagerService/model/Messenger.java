979ee78d72d040ac5feacd84e79dd5af00cac0ff
lakhoune <a.b.lakhoune@gmail.com>
2023-10-23 13:55:58 +0200

    remove old monitoring messages

--- social-bot-manager/src/main/java/i5/las2peer/services/socialBotManagerService/model/Messenger.java
+++ social-bot-manager/src/main/java/i5/las2peer/services/socialBotManagerService/model/Messenger.java
@@ -403,17 +403,7 @@ public class Messenger {
 				remarks.put("user", encryptedUser);
 
 				conversationId = this.determineConversationId(message.getChannel());
-
 				remarks.put("in-service-context", this.triggeredFunction.containsKey(message.getChannel()));
-
-				this.l2pContext.monitorXESEvent(MonitoringEvent.SERVICE_CUSTOM_MESSAGE_1, remarks.toJSONString(),
-						conversationId.toString(),
-						intent.getKeyword(),
-						bot.getId(), "bot", "start", System.currentTimeMillis());
-				this.l2pContext.monitorXESEvent(MonitoringEvent.SERVICE_CUSTOM_MESSAGE_1, remarks.toJSONString(),
-						conversationId.toString(),
-						intent.getKeyword(),
-						bot.getId(), "bot", "complete", System.currentTimeMillis());
 
 				// ________________ start modification of state machine__________________
 
@@ -695,15 +685,6 @@ public class Messenger {
 									}
 
 								}
-								String activityName = state.getIntentKeyword() + ":response";
-								this.l2pContext.monitorXESEvent(MonitoringEvent.SERVICE_CUSTOM_MESSAGE_2,
-										remarks.toJSONString(),
-										conversationId.toString(), activityName, bot.getId(), "bot", "start",
-										System.currentTimeMillis());
-								this.l2pContext.monitorXESEvent(MonitoringEvent.SERVICE_CUSTOM_MESSAGE_2,
-										remarks.toJSONString(),
-										conversationId.toString(), activityName, bot.getId(), "bot", "complete",
-										System.currentTimeMillis());
 								// check if message parses buttons or is simple text
 								if (state.getType().equals("Interactive Message")) {
 									this.chatMediator.sendBlocksMessageToChannel(message.getChannel(), split,
