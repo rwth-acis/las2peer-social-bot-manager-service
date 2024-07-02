6056f984adc3e579fa66241d7e4e1f6ede013cbc
Yinnii <y.yin94@outlook.de>
2024-07-02 13:58:03 +0200

    set context path

--- social-bot-manager/src/main/java/services/socialBotManagerService/SocialBotManagerServiceApplication.java
+++ social-bot-manager/src/main/java/services/socialBotManagerService/SocialBotManagerServiceApplication.java
@@ -1,9 +1,11 @@
 package services.socialBotManagerService;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SocialBotManagerServiceApplication {
     public static void main(String[] args) {
+		System.setProperty("server.servlet.context-path", "/SBFManager");
 		SpringApplication.run(SocialBotManagerServiceApplication.class, args);
 	}
 }
