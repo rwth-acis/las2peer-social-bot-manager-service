b6d8c913e372f3a0c42eae2cc3b82feaec11525b
Yinnii <y.yin94@outlook.de>
2024-08-19 13:07:43 +0200

    update controller

--- social-bot-manager/src/main/java/services/socialBotManagerService/controller/SocialBotManagerServiceController.java
+++ social-bot-manager/src/main/java/services/socialBotManagerService/controller/SocialBotManagerServiceController.java
@@ -12,6 +12,8 @@ import java.net.URL;
 import java.util.Base64;
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -27,10 +29,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
 
-import com.mongodb.client.gridfs.GridFSBucket;
-import com.mongodb.client.gridfs.GridFSBuckets;
-import com.mongodb.client.gridfs.model.GridFSFile;
-import com.mongodb.client.model.Filters;
+// import com.mongodb.client.gridfs.GridFSBucket;
+// import com.mongodb.client.gridfs.GridFSBuckets;
+// import com.mongodb.client.gridfs.model.GridFSFile;
+// import com.mongodb.client.model.Filters;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -46,11 +48,89 @@ import net.minidev.json.parser.ParseException;
 import org.bson.BsonObjectId;
 import org.bson.types.ObjectId;
 
-import javax.ws.rs.core.MediaType;
+import services.socialBotManagerService.nlu.TrainingHelper;
+import services.socialBotManagerService.service.SocialBotManagerService;
 
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-import org.glassfish.jersey.media.multipart.FormDataParam;
+// import javax.ws.rs.core.MediaType;
 
+// import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+// import org.glassfish.jersey.media.multipart.FormDataParam;
+
+
+@Tag(name="SocialBotManagerService", description = "A service for managing social bots.")
+@RestController
+@RequestMapping("/")
 public class SocialBotManagerServiceController {
+    @Autowired
+    private SocialBotManagerService service;
+
+    @Operation(tags = {"trainAndLoad"}, description = "Trains and loads an NLU model on the given Rasa NLU server instance.")
+    @PostMapping(value = "/trainAndLoad", consumes = "application/json", produces = "text/plain")
+	// TODO: Just an adapter, since the Rasa server doesn't support
+	// "Access-Control-Expose-Headers"
+	// and the model file name is returned as a response header... Remove and just
+	// use Rasa's
+	// API directly once that's fixed. The whole `TrainingHelper` class can be
+	// deleted then as well.
+	public ResponseEntity<String> trainAndLoad(String body) {
+		JSONParser p = new JSONParser(JSONParser.MODE_PERMISSIVE);
+		if (service.nluTrainThread != null && service.nluTrainThread.isAlive()) {
+			return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Training still in progress.");
+		}
+		try {
+			JSONObject bodyJson = (JSONObject) p.parse(body);
+			String url = (String) bodyJson.get("url");
+			String config = (String) bodyJson.get("config");
+			String markdownTrainingData = (String) bodyJson.get("markdownTrainingData");
+			String intents = (String) bodyJson.get("intents");
+			// added to have a way to access the intents of the rasa server
+			service.rasaIntents.put(url.split("://")[1], intents);
+			service.nluTrain = new TrainingHelper(url, config, markdownTrainingData);
+			service.nluTrainThread = new Thread(service.nluTrain);
+			service.nluTrainThread.start();
+			// TODO: Create a member for this thread, make another REST method to check
+			// whether
+			// training was successful.
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		// Doesn't signal that training and loading was successful, but that it was
+		// started.
+		return ResponseEntity.ok("Training started.");
+	}
+
+	@Operation(description = "Returns information about the training process started by the last invocation of `/trainAndLoad`.", tags = "")
+	@GetMapping(value = "/trainAndLoadStatus", produces = "text/plain")
+	// TODO: Just an adapter, since the Rasa server doesn't support
+	// "Access-Control-Expose-Headers"
+	// and the model file name is returned as a response header... Remove and just
+	// use Rasa's
+	// API directly once that's fixed. The whole `TrainingHelper` class can be
+	// deleted then as well.
+	public ResponseEntity<String> trainAndLoadStatus(String body) {
+		if (service.nluTrainThread == null) {
+			return ResponseEntity.ok("No training process was started yet.");
+		} else if (service.nluTrainThread.isAlive()) {
+			return ResponseEntity.ok("Training still in progress.");
+		} else if (service.nluTrain.getSuccess()) {
+			return ResponseEntity.ok("Training was successful.");
+		} else {
+			return ResponseEntity.ok("Training failed.");
+		}
+	}
+
+	@Operation(description = "Returns the intents of a current Rasa Model.", tags = "")
+	@GetMapping(value = "/{rasaUrl}/intents", produces = "application/json")
+	public ResponseEntity<String> getIntents(@PathVariable("rasaUrl") String url) {
+		if (service.rasaIntents.get(url) == null) {
+			return ResponseEntity.ok("failed.");
+		} else {
+			String intents = service.rasaIntents.get(url);
+			JSONObject ex = new JSONObject();
+			ex.put("intents", intents);
+			return ResponseEntity.ok(ex.toString());
+		}
+	}
     
 }
