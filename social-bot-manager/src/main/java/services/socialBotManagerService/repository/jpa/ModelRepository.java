26096570169a8e0347eff731a873f043e9540caf
Yinnii <y.yin94@outlook.de>
2024-09-24 16:00:32 +0200

    change bot Id to uuid

--- social-bot-manager/src/main/java/services/socialBotManagerService/repository/jpa/ModelRepository.java
+++ social-bot-manager/src/main/java/services/socialBotManagerService/repository/jpa/ModelRepository.java
@@ -3,10 +3,11 @@ package services.socialBotManagerService.repository.jpa;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import java.util.UUID;
 
 import services.socialBotManagerService.model.Model;
 
-public interface ModelRepository extends JpaRepository<Model, Long> {
+public interface ModelRepository extends JpaRepository<Model, UUID> {
 
     @Query(value = "SELECT m FROM Model m WHERE m.name = :name")
     Model findModelByName(@Param("name") String name);
